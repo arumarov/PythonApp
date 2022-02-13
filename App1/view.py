f557f06561cb474c11330b4319215675ac353a3d
AntonUmarov <arumarov@yandex.ru>
2022-02-13 18:55:02 +0600

    Correct view.py

--- App1/view.py
+++ App1/view.py
@@ -1,6 +1,6 @@
 # Создаем модуль, обеспечивающий взаимодействие с пользователем
 def view_data(data):
-    print(data)
+    print(f'sum = {data}')
 
 def get_value():
     return int(input('value = '))
