65531ca3a00613c81e5db73d54577ea1903de6c6
AntonUmarov <arumarov@yandex.ru>
2022-02-13 19:29:26 +0600

    Correct controller, model_sub

--- App1/controller.py
+++ App1/controller.py
@@ -1,5 +1,5 @@
 # Связывает model.py и view.py
-import model_mult as model # изменяем в данной строке файл, который импортируем
+import model_sub as model # изменяем в данной строке файл, который импортируем
 import view # импортируем view
 
 # создаем метод, создающий "кнопку", которую сможет "нажимать" пользователь
@@ -8,4 +8,4 @@ def button_click():
     value_b = view.get_value()
     model.init(value_a, value_b) # производим инициализацию начальных значений
     result = model.do_it()
-    view.view_data(result, "mult")
+    view.view_data(result, "result")
