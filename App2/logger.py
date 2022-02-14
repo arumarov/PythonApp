90fb2ceef6a7609751644284520fe317b68e7a80
AntonUmarov <arumarov@yandex.ru>
2022-02-14 17:29:17 +0600

    Launch program

--- App2/logger.py
+++ App2/logger.py
@@ -4,17 +4,17 @@ from time import time
 def temperature_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{}; temperature; {}'
+        file.write('{}; temperature; {}\n'
                      .format(time, data))
 
 def pressure_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{}; pressure; {}'
+        file.write('{}; pressure; {}\n'
                      .format(time, data))
 
 def wind_speed_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{}; wind_speed; {}'
+        file.write('{}; wind_speed; {}\n'
                      .format(time, data))
