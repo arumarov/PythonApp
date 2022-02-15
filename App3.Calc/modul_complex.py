e01a5e632959f1bd8b71d6a04f9bffe1bd7b8b94
AntonUmarov <arumarov@yandex.ru>
2022-02-15 10:12:11 +0600

    Fill modul_complex.py

--- App3.Calc/modul_complex.py
+++ App3.Calc/modul_complex.py
@@ -1,1 +1,11 @@
-def complex_
+def complex_add(a, b):
+    return a + b
+
+def complex_sub(a, b):
+    return a - b
+
+def complex_mult(a, b):
+    return a * b
+
+def complex_divid(a, b):
+    return a / b
